867ddf645d1fec1c919ba6a6d892c69b088ac29d
Pedro Lima <tsc.pedrolima@gmail.com>
2024-09-11 15:44:24 -0300

    feat(users): create module, controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://tscpedrolima:bX96sS7Mq3V6kGIa@cluster0.uvhjo.mongodb.net/',
+    ),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
